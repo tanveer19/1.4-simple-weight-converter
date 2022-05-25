3a31fbe61d3fd82f32c381ee4cb8dce0c902f460
tanveer19 <thjbd19@gmail.com>
2022-05-25 22:20:15 +0600

    removed pound to pound and kg to kg conversion

--- main.js
+++ main.js
@@ -14,31 +14,32 @@ document.querySelector("#select").addEventListener("input", function (e) {
   if (e.target.value === "lbKg") {
     input.disabled = false;
     input.setAttribute("placeholder", "Enter pounds");
+    lbs.parentElement.style.display = "none";
+    kgs.parentElement.style.display = "inherit";
 
     //conversions
     input.addEventListener("input", function (e) {
       outputDiv.style.visibility = "visible";
       let pound = e.target.value;
-      grams.innerText = pound * 453.592;
+      grams.innerText = Math.round(pound * 453.592).toFixed(2);
       oz.innerHTML = pound * 16;
-      lbs.innerHTML = pound * 1;
-      kgs.innerHTML = pound * 0.453592;
-      // lbs.parentElement.remove();
+      kgs.innerHTML = Math.round(pound * 0.453592).toFixed(2);
     });
 
     // if kilograms to pounds
   } else if (e.target.value === "kgLb") {
     input.disabled = false;
     input.setAttribute("placeholder", "Enter kilograms");
+    lbs.parentElement.style.display = "inherit";
+    kgs.parentElement.style.display = "none";
 
     //conversions
     input.addEventListener("input", function (e) {
       outputDiv.style.visibility = "visible";
       let kilogram = e.target.value;
       grams.innerText = kilogram * 1000;
-      oz.innerHTML = kilogram * 35.274;
-      lbs.innerHTML = kilogram * 2.20462;
-      kgs.innerHTML = kilogram * 1;
+      oz.innerHTML = Math.round(kilogram * 35.274).toFixed(2);
+      lbs.innerHTML = Math.round(kilogram * 2.20462).toFixed(2);
     });
 
     // if nothing selected
